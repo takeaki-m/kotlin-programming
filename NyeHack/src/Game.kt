292d6f2630c9fffbfe9ef3b394ca63d6fb00dd68
TakeakiMiyazawa <takeakimiyazawa0614@gmail.com>
2022-09-15 00:16:52 +0900

    mod: 範囲指定のif/elseをwhenで置き換え

--- NyeHack/src/Game.kt
+++ NyeHack/src/Game.kt
@@ -18,25 +18,36 @@ fun main(args: Array<String>) {
 
     // if (healthPoints == 100) {
     // こんな書き方ができるのか、意外。変数の中身を指定できる
-    val healthStatus = if (healthPoints == 100) {
-        " is in excellent condition"
-
-//    } else if (healthPoints >= 90){
-    } else if (healthPoints in 90..99){
-        " has a few scratches"
-//    }else if (healthPoints >= 75){
-    }else if (healthPoints in 75..89){
-        // println(name + " has some minor wounds")
-        if (isBlessed) {
-           " has some minor wounds but is healing quite quickly!"
+//    val healthStatus = if (healthPoints == 100) {
+//        " is in excellent condition"
+//
+////    } else if (healthPoints >= 90){
+//    } else if (healthPoints in 90..99){
+//        " has a few scratches"
+////    }else if (healthPoints >= 75){
+//    }else if (healthPoints in 75..89){
+//        // println(name + " has some minor wounds")
+//        if (isBlessed) {
+//           " has some minor wounds but is healing quite quickly!"
+//        } else {
+//            " has some minor wounds."
+//        }
+//    }else if (healthPoints in 17..74){
+//    // }else if (healthPoints >= 15){
+//        " looks pretty hurt"
+//    }else {
+//        " is in awful condition"
+//    }
+    val healthStatus = when (healthPoints) {
+        100 -> " is in excellent condition"
+        in 90..99 -> " has a few scratches"
+        in 75..89 -> if (isBlessed) {
+            " has some minor wounds but is healing quite quickly!"
         } else {
             " has some minor wounds."
         }
-    }else if (healthPoints in 17..74){
-    // }else if (healthPoints >= 15){
-        " looks pretty hurt"
-    }else {
-        " is in awful condition"
+        in 17..74 -> " looks pretty hurt"
+        else -> " is in awful condition"
     }
 
     println(name + " " + healthStatus)
